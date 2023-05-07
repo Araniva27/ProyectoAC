1e466500a3a2a5da715e56cd2dd32f9089517340
fabs1902 <36040836+fabs1902@users.noreply.github.com>
2023-05-06 21:04:27 -0600

    Add

---
+++ SistemaAC/Componentes/CartaHojaTrabajoFinalizadas.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace AutoControl.Componentes
+{
+    partial class CartaHojaTrabajoFinalizadas
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelIndicador = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGenerarPDF = new System.Windows.Forms.Button();
+            this.lblCodigoHT = new System.Windows.Forms.Label();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.lblAsesor = new System.Windows.Forms.Label();
+            this.lblFechaSalida = new System.Windows.Forms.Label();
+            this.lblFechaFinalizacion = new System.Windows.Forms.Label();
+            this.sfdGuardarPDF = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // panelIndicador
+            // 
+            this.panelIndicador.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.panelIndicador.Location = new System.Drawing.Point(0, 0);
+            this.panelIndicador.Name = "panelIndicador";
+            this.panelIndicador.Size = new System.Drawing.Size(25, 219);
+            this.panelIndicador.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Código hoja de trabajo :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(31, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(162, 20);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Fecha de finalización:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(31, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 20);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Cliente:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(31, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Fecha de salida:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Asesor:";
+            // 
+            // btnGenerarPDF
+            // 
+            this.btnGenerarPDF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerarPDF.Location = new System.Drawing.Point(232, 166);
+            this.btnGenerarPDF.Name = "btnGenerarPDF";
+            this.btnGenerarPDF.Size = new System.Drawing.Size(156, 40);
+            this.btnGenerarPDF.TabIndex = 19;
+            this.btnGenerarPDF.Text = "Generar PDF";
+            this.btnGenerarPDF.UseVisualStyleBackColor = true;
+            this.btnGenerarPDF.Click += new System.EventHandler(this.btnGenerarPDF_Click);
+            // 
+            // lblCodigoHT
+            // 
+            this.lblCodigoHT.AutoSize = true;
+            this.lblCodigoHT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigoHT.Location = new System.Drawing.Point(228, 21);
+            this.lblCodigoHT.Name = "lblCodigoHT";
+            this.lblCodigoHT.Size = new System.Drawing.Size(19, 20);
+            this.lblCodigoHT.TabIndex = 20;
+            this.lblCodigoHT.Text = "--";
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCliente.Location = new System.Drawing.Point(228, 54);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(19, 20);
+            this.lblCliente.TabIndex = 21;
+            this.lblCliente.Text = "--";
+            // 
+            // lblAsesor
+            // 
+            this.lblAsesor.AutoSize = true;
+            this.lblAsesor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAsesor.Location = new System.Drawing.Point(228, 84);
+            this.lblAsesor.Name = "lblAsesor";
+            this.lblAsesor.Size = new System.Drawing.Size(19, 20);
+            this.lblAsesor.TabIndex = 22;
+            this.lblAsesor.Text = "--";
+            // 
+            // lblFechaSalida
+            // 
+            this.lblFechaSalida.AutoSize = true;
+            this.lblFechaSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaSalida.Location = new System.Drawing.Point(228, 115);
+            this.lblFechaSalida.Name = "lblFechaSalida";
+            this.lblFechaSalida.Size = new System.Drawing.Size(19, 20);
+            this.lblFechaSalida.TabIndex = 23;
+            this.lblFechaSalida.Text = "--";
+            // 
+            // lblFechaFinalizacion
+            // 
+            this.lblFechaFinalizacion.AutoSize = true;
+            this.lblFechaFinalizacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaFinalizacion.Location = new System.Drawing.Point(228, 143);
+            this.lblFechaFinalizacion.Name = "lblFechaFinalizacion";
+            this.lblFechaFinalizacion.Size = new System.Drawing.Size(19, 20);
+            this.lblFechaFinalizacion.TabIndex = 24;
+            this.lblFechaFinalizacion.Text = "--";
+            // 
+            // sfdGuardarPDF
+            // 
+            this.sfdGuardarPDF.Filter = "Archivos PDF(*.pdf)|*.pdf";
+            // 
+            // CartaHojaTrabajoFinalizadas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.lblFechaFinalizacion);
+            this.Controls.Add(this.lblFechaSalida);
+            this.Controls.Add(this.lblAsesor);
+            this.Controls.Add(this.lblCliente);
+            this.Controls.Add(this.lblCodigoHT);
+            this.Controls.Add(this.btnGenerarPDF);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelIndicador);
+            this.Name = "CartaHojaTrabajoFinalizadas";
+            this.Size = new System.Drawing.Size(663, 216);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelIndicador;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGenerarPDF;
+        private System.Windows.Forms.Label lblCodigoHT;
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.Label lblAsesor;
+        private System.Windows.Forms.Label lblFechaSalida;
+        private System.Windows.Forms.Label lblFechaFinalizacion;
+        private System.Windows.Forms.SaveFileDialog sfdGuardarPDF;
+    }
+}
---
+++ SistemaAC/Componentes/CartaHojaTrabajoFinalizadas.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iText.Kernel.Colors;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Draw;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+
+namespace AutoControl.Componentes
+{
+    public partial class CartaHojaTrabajoFinalizadas : UserControl
+    {
+        public CartaHojaTrabajoFinalizadas()
+        {
+            InitializeComponent();
+        }
+
+        private int _idHt;
+        private string _cliente;
+        private DateTime _fechaSalida;
+        private string _codigoHoja;
+        private string _observacion;
+        private int _colorIndicador;
+        private DateTime _fechaFinalizacion;
+        private string _asesor;
+
+        [Category("Custom props")]
+        public int IdHt
+        {
+            get { return _idHt; }
+            set { _idHt = value; }
+        }
+
+        [Category("Custom props")]
+        public string Cliente
+        {
+            get { return _cliente; }
+            set { _cliente = value; lblCliente.Text = value; }
+        }
+
+        [Category("Custom props")]
+        public DateTime FechaSalida
+        {
+            get { return _fechaSalida; }
+            set { _fechaSalida = value; lblFechaSalida.Text = value.ToString(); }
+        }
+
+        [Category("Custom props")]
+        public string CodigoHoja
+        {
+            get { return _codigoHoja; }
+            set { _codigoHoja = value; lblCodigoHT.Text = value; }
+        }
+
+        [Category("Custom props")]
+        public string Observacion
+        {
+            get { return _observacion; }
+            set { _observacion = value; }
+        }
+
+        [Category("Custom props")]
+        public int ColorIndicador
+        {
+            get { return _colorIndicador; }
+            set
+            {
+                _colorIndicador = value;
+                if (_colorIndicador == 1)
+                {
+                    panelIndicador.BackColor = System.Drawing.Color.Yellow;
+                }
+                else
+                {
+                    panelIndicador.BackColor = System.Drawing.Color.Red;
+                }
+            }
+        }
+
+        [Category("Custom props")]
+        public DateTime FechaFinalizacion
+        {
+            get { return _fechaFinalizacion; }
+            set { _fechaFinalizacion = value; lblFechaFinalizacion.Text = value.ToString(); }
+        }
+
+        [Category("Custom props")]
+        public string Asesor
+        {
+            get { return _asesor; }
+            set { _asesor = value; lblAsesor.Text = value; }
+        }
+
+        private void crearPDF(int idHT)
+        {
+            PdfWriter pdfWriter = new PdfWriter(direccion);
+            PdfDocument pdf = new PdfDocument(pdfWriter);
+            Document documento = new Document(pdf, PageSize.LETTER);
+
+            documento.SetMargins(20, 20, 55, 20);
+
+            Paragraph header = new Paragraph("Reporte \"Hoja de trabajo finalizada\" ")
+                .SetTextAlignment(TextAlignment.CENTER)
+                .SetFontSize(16);
+            LineSeparator ls = new LineSeparator(new SolidLine());
+
+            Table tablaDatos = new Table(1).UseAllAvailableWidth();
+           
+            LineSeparator ls2 = new LineSeparator(new SolidLine());
+
+            Clases.HojaTrabajo objOC = new Clases.HojaTrabajo();
+            objOC.IdHoja = IdHt;
+            objOC.obtenerDatosHT(tablaDatos);
+
+            Table tabla = new Table(4).UseAllAvailableWidth().SetMarginTop(10);
+            Cell celdaTabla1 = new Cell().SetTextAlignment(TextAlignment.CENTER).SetFontSize(11)
+              .Add(new Paragraph("Código del producto")).SetBackgroundColor(ColorConstants.BLACK).SetFontColor(ColorConstants.WHITE);
+            Cell celdaTabla2 = new Cell().SetTextAlignment(TextAlignment.CENTER).SetFontSize(11)
+              .Add(new Paragraph("Cantidad")).SetBackgroundColor(ColorConstants.BLACK).SetFontColor(ColorConstants.WHITE);
+            Cell celdaTabla3 = new Cell().SetTextAlignment(TextAlignment.CENTER).SetFontSize(11)
+              .Add(new Paragraph("Descripción")).SetBackgroundColor(ColorConstants.BLACK).SetFontColor(ColorConstants.WHITE);
+            Cell celdaTabla4 = new Cell().SetTextAlignment(TextAlignment.CENTER).SetFontSize(11)
+              .Add(new Paragraph("Nombre del producto")).SetBackgroundColor(ColorConstants.BLACK).SetFontColor(ColorConstants.WHITE);
+
+
+            Clases.HojaTrabajo objOC2 = new Clases.HojaTrabajo();
+            objOC2.IdHoja = idHT;
+            objOC2.obtenerDetallesHT(tabla);
+
+            documento.Add(header);
+            documento.Add(ls);
+            
+            tabla.AddHeaderCell(celdaTabla2);
+            tabla.AddHeaderCell(celdaTabla1);
+            tabla.AddHeaderCell(celdaTabla4);
+            tabla.AddHeaderCell(celdaTabla3);
+            documento.Add(tablaDatos);
+            documento.Add(ls2);
+            documento.Add(tabla);
+
+            //Número de paginas
+            int numeroPaginas = pdf.GetNumberOfPages();
+            for (int i = 1; i <= numeroPaginas; i++)
+            {
+                documento.ShowTextAligned(new Paragraph(string
+                   .Format("Página " + i + " de " + numeroPaginas)),
+                    580, 10, i, TextAlignment.RIGHT,
+                    VerticalAlignment.BOTTOM, 0);
+            }
+            
+            documento.Close();
+
+
+        }
+
+        string direccion = "";
+        private void btnGenerarPDF_Click(object sender, EventArgs e)
+        {            
+            sfdGuardarPDF.FileName = "Reporte hoja de trabajo";
+            if (sfdGuardarPDF.ShowDialog() == DialogResult.OK)
+            {
+                direccion = sfdGuardarPDF.FileName;
+                crearPDF(IdHt);
+                MessageBox.Show("Se ha creado el PDF de la orden de compra exitosamente", "VERIFICAR", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}

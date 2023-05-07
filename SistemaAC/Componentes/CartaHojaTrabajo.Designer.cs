b92678e4e886f6892c2951100dae44e5c20de8df
Araniva27 <manuelaraniva07@gmail.com>
2023-05-06 21:05:16 -0600

    Componentes

---
+++ SistemaAC/Componentes/CartaHojaTrabajo.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace AutoControl.Componentes
+{
+    partial class CartaHojaTrabajo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelIndicador = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnDetallesHT = new System.Windows.Forms.Button();
+            this.btnFinalizarHT = new System.Windows.Forms.Button();
+            this.lblCodigoHT = new System.Windows.Forms.Label();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.lblAsesor = new System.Windows.Forms.Label();
+            this.lblFechaSalida = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblFechaRegistro = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panelIndicador
+            // 
+            this.panelIndicador.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(10)))), ((int)(((byte)(35)))));
+            this.panelIndicador.Location = new System.Drawing.Point(0, 0);
+            this.panelIndicador.Name = "panelIndicador";
+            this.panelIndicador.Size = new System.Drawing.Size(25, 219);
+            this.panelIndicador.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Código hoja de trabajo :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Asesor:";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(31, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Fecha de salida:";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(31, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Cliente:";
+            // 
+            // btnDetallesHT
+            // 
+            this.btnDetallesHT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDetallesHT.Location = new System.Drawing.Point(151, 166);
+            this.btnDetallesHT.Name = "btnDetallesHT";
+            this.btnDetallesHT.Size = new System.Drawing.Size(193, 35);
+            this.btnDetallesHT.TabIndex = 9;
+            this.btnDetallesHT.Text = "Ver detalles";
+            this.btnDetallesHT.UseVisualStyleBackColor = true;
+            this.btnDetallesHT.Click += new System.EventHandler(this.btnDetallesHT_Click);
+            // 
+            // btnFinalizarHT
+            // 
+            this.btnFinalizarHT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFinalizarHT.Location = new System.Drawing.Point(350, 166);
+            this.btnFinalizarHT.Name = "btnFinalizarHT";
+            this.btnFinalizarHT.Size = new System.Drawing.Size(193, 35);
+            this.btnFinalizarHT.TabIndex = 10;
+            this.btnFinalizarHT.Text = "Finalizar Hoja de trabajo";
+            this.btnFinalizarHT.UseVisualStyleBackColor = true;
+            this.btnFinalizarHT.Click += new System.EventHandler(this.btnFinalizarHT_Click);
+            // 
+            // lblCodigoHT
+            // 
+            this.lblCodigoHT.AutoSize = true;
+            this.lblCodigoHT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigoHT.Location = new System.Drawing.Point(237, 12);
+            this.lblCodigoHT.Name = "lblCodigoHT";
+            this.lblCodigoHT.Size = new System.Drawing.Size(19, 20);
+            this.lblCodigoHT.TabIndex = 7;
+            this.lblCodigoHT.Text = "--";
+            this.lblCodigoHT.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCliente.Location = new System.Drawing.Point(237, 43);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(19, 20);
+            this.lblCliente.TabIndex = 11;
+            this.lblCliente.Text = "--";
+            // 
+            // lblAsesor
+            // 
+            this.lblAsesor.AutoSize = true;
+            this.lblAsesor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAsesor.Location = new System.Drawing.Point(237, 73);
+            this.lblAsesor.Name = "lblAsesor";
+            this.lblAsesor.Size = new System.Drawing.Size(19, 20);
+            this.lblAsesor.TabIndex = 12;
+            this.lblAsesor.Text = "--";
+            // 
+            // lblFechaSalida
+            // 
+            this.lblFechaSalida.AutoSize = true;
+            this.lblFechaSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaSalida.Location = new System.Drawing.Point(237, 104);
+            this.lblFechaSalida.Name = "lblFechaSalida";
+            this.lblFechaSalida.Size = new System.Drawing.Size(19, 20);
+            this.lblFechaSalida.TabIndex = 13;
+            this.lblFechaSalida.Text = "--";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(31, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(137, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Fecha de registro:";
+            // 
+            // lblFechaRegistro
+            // 
+            this.lblFechaRegistro.AutoSize = true;
+            this.lblFechaRegistro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaRegistro.Location = new System.Drawing.Point(237, 134);
+            this.lblFechaRegistro.Name = "lblFechaRegistro";
+            this.lblFechaRegistro.Size = new System.Drawing.Size(19, 20);
+            this.lblFechaRegistro.TabIndex = 15;
+            this.lblFechaRegistro.Text = "--";
+            // 
+            // CartaHojaTrabajo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.lblFechaRegistro);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblFechaSalida);
+            this.Controls.Add(this.lblAsesor);
+            this.Controls.Add(this.lblCliente);
+            this.Controls.Add(this.btnFinalizarHT);
+            this.Controls.Add(this.btnDetallesHT);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCodigoHT);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelIndicador);
+            this.Name = "CartaHojaTrabajo";
+            this.Size = new System.Drawing.Size(665, 218);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelIndicador;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnDetallesHT;
+        private System.Windows.Forms.Button btnFinalizarHT;
+        private System.Windows.Forms.Label lblCodigoHT;
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.Label lblAsesor;
+        private System.Windows.Forms.Label lblFechaSalida;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblFechaRegistro;
+    }
+}
---
+++ SistemaAC/Componentes/CartaHojaTrabajo.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoControl.Componentes
+{
+    public partial class CartaHojaTrabajo : UserControl
+    {
+        public CartaHojaTrabajo()
+        {
+            InitializeComponent();
+        }
+
+        private int _idHt;
+        private string _cliente;
+        private DateTime _fechaSalida;
+        private string _codigoHoja;
+        private string _observacion;
+        private int _colorIndicador;
+        private DateTime _fechaRegistro;
+        private string _asesor;
+
+        [Category("Custom props")]
+        public int IdHt
+        {
+            get { return _idHt; }
+            set { _idHt = value; }
+        }
+
+        [Category("Custom props")]
+        public  string Cliente
+        {
+            get { return _cliente; }
+            set { _cliente = value; lblCliente.Text = value; }
+        }
+
+        [Category("Custom props")]
+        public DateTime FechaSalida
+        {
+            get { return _fechaSalida; }
+            set { _fechaSalida = value; lblFechaSalida.Text = value.ToString(); }
+        }
+
+        [Category("Custom props")]
+        public string CodigoHoja
+        {
+            get { return _codigoHoja; }
+            set { _codigoHoja = value; lblCodigoHT.Text = value; }
+        }
+
+        [Category("Custom props")]
+        public string Observacion
+        {
+            get { return _observacion; }
+            set { _observacion = value; }
+        }
+
+        [Category("Custom props")]
+        public int ColorIndicador
+        {
+            get { return _colorIndicador; }
+            set { _colorIndicador = value; 
+                if(_colorIndicador == 1)
+                {
+                    panelIndicador.BackColor = Color.Yellow;
+                }
+                else
+                {
+                    panelIndicador.BackColor = Color.Red;
+                }
+            }
+        }
+
+        [Category("Custom props")]
+        public DateTime FechaRegistro
+        {
+            get { return _fechaRegistro; }
+            set { _fechaRegistro = value; lblFechaRegistro.Text = value.ToString(); }
+        }
+
+        [Category("Custom props")]
+        public string Asesor
+        {
+            get { return _asesor; }
+            set { _asesor = value; lblAsesor.Text = value; }
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFinalizarHT_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("¿Seguro que desea finalizar la hoja de trabajo?", "ATENCIÓN", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                Clases.HojaTrabajo objHT = new Clases.HojaTrabajo();
+                objHT.IdHoja = IdHt;
+                if (objHT.finalizarHT())
+                {                    
+                    if (objHT.entregarProductosHT())
+                    {
+                        MessageBox.Show("Hoja de trabajo finalizada correctamente", "ÉXITO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error al entregar productos de la hoja de trabajo, comuníquese con el administrador", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error al finalizar hoja de trabajo, comuníquese con el administrador", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No se finalizará la hoja de trabajo", "INFORMACIÓN", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnDetallesHT_Click(object sender, EventArgs e)
+        {
+            Forms.DetalleHojaTrabajocs frmDetalleHT = new Forms.DetalleHojaTrabajocs(_idHt);
+            frmDetalleHT.Show();
+            ParentForm.Hide();
+        }
+    }
+}

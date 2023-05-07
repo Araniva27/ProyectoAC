bcd4fc2e70b5be2f9ba7df57bf91f685864bf6b3
Ortega02 <89029520+Ortega02@users.noreply.github.com>
2023-05-06 21:10:30 -0600

    Componentes CartaProductoHojaTrabajo

---
+++ SistemaAC/Componentes/CartaProductoHojaTrabajo.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace AutoControl.Componentes
+{
+    partial class CartaProductoHojaTrabajo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMarcarReingreso = new System.Windows.Forms.Button();
+            this.lblDescripción = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblNombreProducto = new System.Windows.Forms.Label();
+            this.lblCodigoProducto = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelIndicador = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnMarcarReingreso
+            // 
+            this.btnMarcarReingreso.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMarcarReingreso.Location = new System.Drawing.Point(213, 202);
+            this.btnMarcarReingreso.Name = "btnMarcarReingreso";
+            this.btnMarcarReingreso.Size = new System.Drawing.Size(276, 35);
+            this.btnMarcarReingreso.TabIndex = 13;
+            this.btnMarcarReingreso.Text = "Marcar producto como reingresado";
+            this.btnMarcarReingreso.UseVisualStyleBackColor = true;
+            this.btnMarcarReingreso.Click += new System.EventHandler(this.btnMarcarReingreso_Click);
+            // 
+            // lblDescripción
+            // 
+            this.lblDescripción.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripción.Location = new System.Drawing.Point(209, 111);
+            this.lblDescripción.Name = "lblDescripción";
+            this.lblDescripción.Size = new System.Drawing.Size(451, 79);
+            this.lblDescripción.TabIndex = 9;
+            this.lblDescripción.Text = "--";
+            this.lblDescripción.Click += new System.EventHandler(this.lblDescripción_Click);
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidad.Location = new System.Drawing.Point(209, 79);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(451, 20);
+            this.lblCantidad.TabIndex = 10;
+            this.lblCantidad.Text = "--";
+            // 
+            // lblNombreProducto
+            // 
+            this.lblNombreProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreProducto.Location = new System.Drawing.Point(209, 46);
+            this.lblNombreProducto.Name = "lblNombreProducto";
+            this.lblNombreProducto.Size = new System.Drawing.Size(451, 20);
+            this.lblNombreProducto.TabIndex = 11;
+            this.lblNombreProducto.Text = "--";
+            // 
+            // lblCodigoProducto
+            // 
+            this.lblCodigoProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigoProducto.Location = new System.Drawing.Point(209, 14);
+            this.lblCodigoProducto.Name = "lblCodigoProducto";
+            this.lblCodigoProducto.Size = new System.Drawing.Size(451, 20);
+            this.lblCodigoProducto.TabIndex = 12;
+            this.lblCodigoProducto.Text = "--";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(34, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Descripción:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(34, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Cantidad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nombre del producto:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Código del producto:";
+            // 
+            // panelIndicador
+            // 
+            this.panelIndicador.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(10)))), ((int)(((byte)(35)))));
+            this.panelIndicador.Location = new System.Drawing.Point(0, 0);
+            this.panelIndicador.Name = "panelIndicador";
+            this.panelIndicador.Size = new System.Drawing.Size(28, 267);
+            this.panelIndicador.TabIndex = 14;
+            // 
+            // CartaProductoHojaTrabajo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.panelIndicador);
+            this.Controls.Add(this.btnMarcarReingreso);
+            this.Controls.Add(this.lblDescripción);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.lblNombreProducto);
+            this.Controls.Add(this.lblCodigoProducto);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CartaProductoHojaTrabajo";
+            this.Size = new System.Drawing.Size(696, 257);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMarcarReingreso;
+        private System.Windows.Forms.Label lblDescripción;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Label lblNombreProducto;
+        private System.Windows.Forms.Label lblCodigoProducto;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelIndicador;
+    }
+}
---
+++ SistemaAC/Componentes/CartaProductoHojaTrabajo.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoControl.Componentes
+{
+    public partial class CartaProductoHojaTrabajo : UserControl
+    {
+        public CartaProductoHojaTrabajo()
+        {
+            InitializeComponent();
+        }
+
+        private string _codigoProducto;
+        private string _nombreProducto;
+        private int _cantidad;
+        private string _descripcion;
+        private int _estadoEntrada;
+        private int _idDetalleHT;
+        private int _idHojaTrabajo;
+
+        public string CodigoProducto
+        {
+            get { return _codigoProducto; }
+            set { _codigoProducto = value; lblCodigoProducto.Text = value; }
+        }
+        
+        public  string NombreProducto
+        {
+            get { return _nombreProducto; }
+            set { _nombreProducto = value; lblNombreProducto.Text = value; }
+        }
+        
+        public int Cantidad
+        {
+            get { return _cantidad; }
+            set { _cantidad = value; lblCantidad.Text = value.ToString(); }
+        }
+
+        public string Descripcion
+        {
+            get { return _descripcion; }
+            set { _descripcion = value; lblDescripción.Text = value; }
+        }
+
+        public int EstadoEntrada
+        {
+            get { return _estadoEntrada; }
+            set
+            {
+                _estadoEntrada = value;
+
+                if (_estadoEntrada == 0)
+                {
+                    panelIndicador.BackColor = Color.Yellow;
+                }
+
+                if (_estadoEntrada == 1)
+                {
+                    panelIndicador.BackColor = Color.Green;
+                    btnMarcarReingreso.Hide();
+                }
+            }
+
+        }
+        
+        public int IdDetalleHT
+        {
+            get { return _idDetalleHT; }
+            set { _idDetalleHT = value; }
+        }
+
+        public int IdHojaTrabajo
+        {
+            get { return _idHojaTrabajo; }
+            set { _idHojaTrabajo = value; }
+        }
+
+
+
+        private void lblDescripción_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMarcarReingreso_Click(object sender, EventArgs e)
+        {
+            Clases.HojaTrabajo objHT = new Clases.HojaTrabajo();
+            objHT.IdDetalleHT = _idDetalleHT;
+            if(MessageBox.Show("¿Desea marcar como entregado el producto?", "ÉXITO", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            {
+                if (objHT.entregarProductoHojaTrabajo())
+                {
+                    MessageBox.Show("Se ha entregado el producto exitosamente", "ÉXITO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    panelIndicador.BackColor = Color.Green;
+                    btnMarcarReingreso.Hide();
+                    objHT.IdHoja = _idHojaTrabajo;
+                    if(objHT.cantidadProductoPendienteEntregaHT() == 0)
+                    {
+                        if (objHT.finalizarHT())
+                        {
+                            MessageBox.Show("Se han entregado todos los productos de la hoja de trabajo, se marcó como finaalizada", "ÉXITO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error al finalizar la hoja de trabajo, comuníquese con el administrador", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }                   
+                }
+                else
+                {
+                    MessageBox.Show("Error al marcar el producto como ingresado, comuniquese con el administrador", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No se marcará como entregado el producto", "INFORMACIÓN", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }                
+        }
+    }
+}

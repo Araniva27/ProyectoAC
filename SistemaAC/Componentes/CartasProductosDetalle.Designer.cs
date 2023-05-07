26b76406d81cca0d47f6a97dac6874dd1cdc45f0
Ortega02 <89029520+Ortega02@users.noreply.github.com>
2023-05-06 21:16:10 -0600

    Componentes CartasProductosDetalle

---
+++ SistemaAC/Componentes/CartasProductosDetalle.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace AutoControl.Componentes
+{
+    partial class CartasProductosDetalle
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelIndicador = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCódigoProducto = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblDescricpcion = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblNombreProducto = new System.Windows.Forms.Label();
+            this.btnEntregarProducto = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panelIndicador
+            // 
+            this.panelIndicador.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(10)))), ((int)(((byte)(35)))));
+            this.panelIndicador.Location = new System.Drawing.Point(0, 0);
+            this.panelIndicador.Name = "panelIndicador";
+            this.panelIndicador.Size = new System.Drawing.Size(20, 441);
+            this.panelIndicador.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Código de producto:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cantidad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Descripción:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblCódigoProducto
+            // 
+            this.lblCódigoProducto.AutoSize = true;
+            this.lblCódigoProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCódigoProducto.Location = new System.Drawing.Point(207, 27);
+            this.lblCódigoProducto.Name = "lblCódigoProducto";
+            this.lblCódigoProducto.Size = new System.Drawing.Size(19, 20);
+            this.lblCódigoProducto.TabIndex = 4;
+            this.lblCódigoProducto.Text = "--";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidad.Location = new System.Drawing.Point(207, 71);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(19, 20);
+            this.lblCantidad.TabIndex = 5;
+            this.lblCantidad.Text = "--";
+            // 
+            // lblDescricpcion
+            // 
+            this.lblDescricpcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescricpcion.Location = new System.Drawing.Point(208, 213);
+            this.lblDescricpcion.Name = "lblDescricpcion";
+            this.lblDescricpcion.Size = new System.Drawing.Size(332, 135);
+            this.lblDescricpcion.TabIndex = 6;
+            this.lblDescricpcion.Text = "--";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(38, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Nombre del producto:";
+            // 
+            // lblNombreProducto
+            // 
+            this.lblNombreProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreProducto.Location = new System.Drawing.Point(207, 127);
+            this.lblNombreProducto.Name = "lblNombreProducto";
+            this.lblNombreProducto.Size = new System.Drawing.Size(333, 63);
+            this.lblNombreProducto.TabIndex = 8;
+            this.lblNombreProducto.Text = "--";
+            // 
+            // btnEntregarProducto
+            // 
+            this.btnEntregarProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntregarProducto.Location = new System.Drawing.Point(162, 375);
+            this.btnEntregarProducto.Name = "btnEntregarProducto";
+            this.btnEntregarProducto.Size = new System.Drawing.Size(235, 38);
+            this.btnEntregarProducto.TabIndex = 9;
+            this.btnEntregarProducto.Text = "Marcar como entregado";
+            this.btnEntregarProducto.UseVisualStyleBackColor = true;
+            this.btnEntregarProducto.Click += new System.EventHandler(this.btnEntregarProducto_Click);
+            // 
+            // CartasProductosDetalle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.btnEntregarProducto);
+            this.Controls.Add(this.lblNombreProducto);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblDescricpcion);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.lblCódigoProducto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelIndicador);
+            this.Name = "CartasProductosDetalle";
+            this.Size = new System.Drawing.Size(556, 440);
+            this.Load += new System.EventHandler(this.CartasProductosDetalle_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelIndicador;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCódigoProducto;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Label lblDescricpcion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblNombreProducto;
+        private System.Windows.Forms.Button btnEntregarProducto;
+    }
+}
---
+++ SistemaAC/Componentes/CartasProductosDetalle.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoControl.Componentes
+{
+    public partial class CartasProductosDetalle : UserControl
+    {
+        public CartasProductosDetalle()
+        {
+            InitializeComponent();
+        }
+
+        private string _codigoProducto;
+        private int _cantidadProducto;
+        private string _descripcion;
+        private string _nombreProducto;
+        private int _idOC;
+        private int _colorIndicador;
+
+        [Category("Custom props")]
+        public string CodigoProducto
+        {
+            get { return _codigoProducto; }
+            set { _codigoProducto = value; lblCódigoProducto.Text = value; }
+        }
+
+        [Category("Custom props")]
+        public int CantidadProducto
+        {
+            get { return _cantidadProducto; }
+            set { _cantidadProducto = value; lblCantidad.Text = Convert.ToString(value); }
+        }
+
+        [Category("Custom props")]
+        public string Descripcion
+        {
+            get { return _descripcion; }
+            set { _descripcion = value; lblDescricpcion.Text = value; }
+        }
+
+        [Category("Custom props")]
+        public string NombreProducto
+        {
+            get { return _nombreProducto; }
+            set { _nombreProducto = value; lblNombreProducto.Text = value; }
+        }
+
+        [Category("Custom props")]
+        public int IdOC
+        {
+            get { return _idOC; }
+            set { _idOC = value; }
+        }
+
+        [Category("Custom props")]
+        public int ColorIndicador
+        {
+            get { return _colorIndicador; }
+            set { _colorIndicador = value; 
+                if(_colorIndicador == 1)
+                {
+                    panelIndicador.BackColor = Color.Green;
+                    btnEntregarProducto.Hide();
+                }
+                else
+                {
+                    panelIndicador.BackColor = Color.Yellow;
+                }
+            }
+        }
+
+
+
+        private void CartasProductosDetalle_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEntregarProducto_Click(object sender, EventArgs e)
+        {
+            Clases.OrdenesCompra objOC = new Clases.OrdenesCompra();
+            objOC.Id_orden_compra = IdOC;
+            objOC.Codigo_producto = CodigoProducto;
+            if (objOC.actualizarEstadoEntregaProducto())
+            {
+                MessageBox.Show("Se ha marcado como entregado el producto", "ÉXITO", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                ColorIndicador = 1;
+                btnEntregarProducto.Hide();
+                if(objOC.obtenerCantidadProductoPendienteEntrega() == 0)
+                {
+                    if (objOC.finalizarOrdenCompra())
+                    {
+                        MessageBox.Show("Se ha entregado todos los productos de la orden de compra, se ha finalizado la orden de compra", "ÉXITO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error al finalizar la orden de compra, comuníquese con el administrador", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error al marcar como entregado el producto, comuníquese con el administriador", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
